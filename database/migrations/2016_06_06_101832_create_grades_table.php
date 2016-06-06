63059f531e7389414b78f21d8c10af673c3751dd
atanl <linyongtan@126.com>
2016-06-06 18:51:44 +0800

    1. 添加等级数据库表结构。

---
+++ database/migrations/2016_06_06_101832_create_grades_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('capacity')->unsigned()->comment('可以创建的员工人数上限');
+            $table->double('price')->unsigned()->comment('这个等级需要的服务费用');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('grades');
+    }
+}
